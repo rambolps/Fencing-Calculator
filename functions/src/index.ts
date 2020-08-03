b5e0f17e475036ee30ef6996c251c88802158bed
Ryan Rambali <ryanrambali@gmail.com>
2020-08-03 17:36:47 -0400

    run every 12 hours cron table

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,25 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import axios from 'axios';
+
 admin.initializeApp();
 const db = admin.firestore();
+
+function getUrls() {
+    db.collection('stores').doc()
+}
+
+function getPrices() {
+    
+}
+
+function updateDB() {
+    
+}
 
 export const priceCheck = functions.pubsub
 
     .schedule('0 */12 * * *').onRun(context => {
         
+
     });
